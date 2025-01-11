39f35395919ba4eecb90f5ad081562d24823a04a
Rohan kumar <46267198+RohanGau@users.noreply.github.com>
2025-01-11 18:14:54 +0530

    Update throttling.js

--- throttling.js
+++ throttling.js
@@ -35,8 +35,82 @@ function throttle(fn, interval) {
   };
 }
 
+ function throttleWithTimer(fn, delay) {
+    let timer = null;
+    return function (...args) {
+      const context = this;
+      if (!timer) {
+        timer = setInterval(() => {
+          fn.apply(context, args);
+          timer = null;
+        }, delay);
+      }
+    };
+  }
+
+
 const throttleUpdateLayout = throttle(updateLayout, 1000);
 
 window.addEventListener("resize", () => {
   throttleUpdateLayout();
 });
+
+
+// important concept of cold down logic
+function throttle(func, wait) {
+    let waiting = false;
+    let lastArgs = null;
+    
+    function startCooling() {
+        setTimeout(() => {
+           if(lastArgs) {
+               func.apply(this, lastArgs);
+               lastArgs = null;
+               startCooling();
+           } else {
+               waiting = false;
+           }
+        }, wait);
+    }
+    
+    return function(...args) {
+        if(!waiting) {
+            func.apply(this, args);
+            waiting = true;
+            startCooling();
+        } else {
+            lastArgs = args;
+        }
+    }
+    
+}
+
+
+// advance throttling logic
+ function advanceThrottle(
+    fn,
+    delay,
+    option = { leading: true, trailig: true }
+  ) {
+    let timer = null;
+    let lastCall = 0;
+    return function (...args) {
+      const now = Date.now();
+      const context = this;
+
+      if (option.leading && now - lastCall >= delay) {
+        lastCall = now;
+        console.log("leading call");
+        fn.apply(context, args);
+      }
+
+      if (option.trailing && !timer) {
+        timer = setInterval(() => {
+          timer = null;
+          lastCall = option.leading ? Date.now() : lastCall;
+          console.log("trailing call");
+          fn.apply(context, args);
+        }, delay);
+      }
+    };
+  }
