c05195f80027d958d3477d5c1db986fd00dc30ad
Rohan kumar <46267198+RohanGau@users.noreply.github.com>
2025-02-10 01:28:53 +0530

    Update eventEmitter.js

--- eventEmitter.js
+++ eventEmitter.js
@@ -29,3 +29,60 @@ class EventEmitter {
     }
   }
 }
+
+
+
+// more enhancement
+
+class EventEmitter {
+    constructor() {
+        this.event = {};
+    }
+    
+    emit(eventName, ...args) {
+        if(!this.event[eventName] || this.event[eventName].length === 0) {
+            console.warn(`Event ${eventName} not found or has no listeners`)
+            return;
+        }
+
+        this.event[eventName].forEach((callback) => {
+            callback.call(this, ...args);
+        });
+    }
+    
+    subscribe(eventName, callback) {
+        if(!this.event[eventName]) {
+            this.event[eventName] = [];
+        }
+        
+        if(!this.event[eventName].includes(callback)) {
+            this.event[eventName].push(callback);
+        }
+        
+        
+        return {
+            remove: () => {
+                this.event[eventName] = this.event[eventName].filter((fn) => fn !== callback);
+                if (this.event[eventName].length === 0) {
+                   delete this.event[eventName];
+                }
+            }
+        }
+    }
+}
+
+const emitter = new EventEmitter();
+
+const subscription = emitter.subscribe("modify", (link) => {
+    console.log(`Modified : ${link}`);
+});
+
+emitter.emit("modify", "test@gmail.com");
+subscription.remove();
+
+emitter.emit("modify", "rohan@gmail.com");
+// // no event found
+emitter.emit("notEventFound", "test@gmail.com");
+
+
+
